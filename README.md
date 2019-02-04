559100424a3098e127dae8c8d9706a10528bf3d2
vladimirmalcevic@ZANZIBAR <vladimirmalcevic@gmail.com>
2019-02-04 14:40:09 -0500

    Finished Back End
    Back end file for game finished and tested. toString function added to see state of game board and Main function used to set, swipe, and reset board for different directions.

--- README.md
+++ README.md
@@ -9,13 +9,12 @@ Create a hexagonal version of the popular app game 2048 in Android Studio.
 The back end of the game will be accomplished through use of a map, and a 2-D with side of size five. The map will save the base state of the array and the 2-D array with side of size 5 will be used to collapse the values in the board on swipe. Depending on the swipe direction, the map will load different nodes into the array to ensure that the 2-D array works for all swipe cases.
 
 ### Project Goals: ###
-- [ ] Write the map function to save state of the board
-- [ ] Write the function to load state of map into 2-D array for different direction of collapse
-- [ ] Write the function to collapse the 2-D array
-- [ ] Write the function to load the state of the 2-D array into the map
+- [x] Write the map function to save state of the board
+- [x] Write the function to load state of map into 2-D array for different direction of collapse
+- [x] Write the function to collapse the 2-D array
+- [x] Write the function to load the state of the 2-D array into the map
 - [ ] Construct the visual representation of the game board
 - [ ] Accomplish functionality of game using buttons
 - [ ] Add animation to the change of state of the game board
 - [ ] Change functionality of game from pushing buttons to swiping the screen
 - [ ] Add music to the game and a mute button to toggle if music is played
-
--- app/src/main/java/com/example/thisisnt2048anymore/HexGame.java
+++ app/src/main/java/com/example/thisisnt2048anymore/HexGame.java
@@ -1,7 +1,10 @@
 package com.example.thisisnt2048anymore;
 
+import android.os.SystemClock;
+
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -33,9 +36,9 @@ public class HexGame {
 
     private static final int[][] STARTING_NODES = {to_up, to_left_up, to_left_down, to_down, to_right_down, to_right_up};
 
-    HashMap<Integer, Integer> game_board = new HashMap<>();
+    public HashMap<Integer, Integer> game_board = new HashMap<>();
 
-    Queue<Integer> tile_changes = new LinkedList<Integer>();
+    public Queue<Integer> tile_changes = new LinkedList<Integer>();
 
     public HexGame() {
         for (int i = 0; i < NUMBER_NODES; i++) {
@@ -44,58 +47,99 @@ public class HexGame {
     }
 
     private int[][] loadIntoArray(int direction){
+        int[] array_size = {3,4,5,4,3};
         int[][] board_state = new int[5][5];
         int progression_start = 0;
         int progression_end = 1;
         int progression_index;
         int progression_direction = (direction < 3) ? 1 : -1 ;
-        for (int i = 0; i < 5; i++) {
-            board_state[i][0] = this.game_board.get(STARTING_NODES[direction][i]);
-        }
+        int next_node;
+
         for (int i = 0; i < 5; i++) {
             progression_index = progression_start;
-            for (int j = 0; ((((i==0)||(i==4))&&(j<3))||(((i==1)||(i==3))&&(j<4))||((i==2)&&(j<5))); j++) {
-                board_state[i][j] = this.game_board.get(STARTING_NODES[direction][(board_state[i][j-1]) + (progression_direction * PROGRESSION[direction%3][progression_index])]);
-                progression_index++;
+            next_node = STARTING_NODES[direction][i];
+            for (int j = 0; j < array_size[i]; j++) {
+                board_state[i][j] = this.game_board.get(next_node);
+                if(progression_index <= progression_end) {
+                    next_node = next_node + (progression_direction * PROGRESSION[direction % 3][progression_index]);
+                    progression_index++;
+                }
             }
-            if(progression_end == 3){progression_start++;}
-            else{progression_end++;}
+            if(progression_end == 3){ progression_start++; }
+            else{ progression_end++; }
         }
+
+//        System.out.println(Arrays.toString(board_state[0]));
+//        System.out.println(Arrays.toString(board_state[1]));
+//        System.out.println(Arrays.toString(board_state[2]));
+//        System.out.println(Arrays.toString(board_state[3]));
+//        System.out.println(Arrays.toString(board_state[4]));
+
+
         return board_state;
     }
 
     private void loadIntoMap(int direction, int[][] board_state){
+        int[] array_size = {3,4,5,4,3};
         int progression_start = 0;
         int progression_end = 1;
         int progression_index;
         int progression_direction = (direction < 3) ? 1 : -1 ;
-        for (int i = 0; i < 5; i++) {
-            this.game_board.replace(STARTING_NODES[direction][i], board_state[i][0]);
-        }
+        int next_node;
+
         for (int i = 0; i < 5; i++) {
             progression_index = progression_start;
-            for (int j = 0; ((((i==0)||(i==4))&&(j<3))||(((i==1)||(i==3))&&(j<4))||((i==2)&&(j<5))); j++) {
-                this.game_board.replace((STARTING_NODES[direction][(board_state[i][j-1]) + (progression_direction * PROGRESSION[direction%3][progression_index])]), board_state[i][j]);
-                progression_index++;
+            next_node = STARTING_NODES[direction][i];
+            for (int j = 0; j < array_size[i]; j++) {
+                this.game_board.replace(next_node, board_state[i][j]);
+                if(progression_index <= progression_end) {
+                    next_node = next_node + (progression_direction * PROGRESSION[direction % 3][progression_index]);
+                    progression_index++;
+                }
             }
-            if(progression_end == 3){progression_start++;}
-            else{progression_end++;}
+            if(progression_end == 3){ progression_start++; }
+            else{ progression_end++; }
         }
     }
 
     private int[][] collapseArray(int direction, int[][] board_state){
-        //TODO: Figure out how to make the board collapse
 
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; ((((i==0)||(i==4))&&(j<3))||(((i==1)||(i==3))&&(j<4))||((i==2)&&(j<5))); j++) {
-
-                if(board_state[i][j] == board_state[i][j+1]){
-                    board_state[i][j] = board_state[i][j] * 2;
-                    board_state[i][j+1] = 0;
-                } else if(board_state[i][j] == 0){
-                    board_state[i][j] = 1;
+        int[] array_size = {3,4,5,4,3};
+        int next_index_fill;
+        int i,j,k;
+        int temp1;
+        for (i = 0; i < 5; i++) {
+            next_index_fill = 0;
+            j = 0;
+            while(j < array_size[i]){
+                if(board_state[i][j]!=0){
+                    k = j + 1;
+                    while((k < array_size[i]) && (board_state[i][k] == 0)){
+                        k++;
+                    }
+                    if(k < array_size[i]){
+                        if(board_state[i][j] == board_state[i][k]){
+                            board_state[i][j] = 0;
+                            board_state[i][next_index_fill] = (board_state[i][k]) * 2;
+                            board_state[i][k] = 0;
+                            next_index_fill++;
+                            j = k + 1;
+                        } else {
+                            temp1 = board_state[i][j];
+                            board_state[i][j] = 0;
+                            board_state[i][next_index_fill] = temp1;
+                            next_index_fill++;
+                            j = k;
+                        }
+                    } else {
+                        temp1 = board_state[i][j];
+                        board_state[i][j] = 0;
+                        board_state[i][next_index_fill] = temp1;
+                        j = k;
+                    }
+                } else {
+                    j++;
                 }
-
             }
         }
         return board_state;
@@ -105,8 +149,96 @@ public class HexGame {
         loadIntoMap(direction, collapseArray(direction,loadIntoArray(direction)));
     }
 
+    public void reset(){
+        for (int i = 0; i < NUMBER_NODES; i++) {
+            this.game_board.put(i, 0);
+        }
+    }
+
 
     private void detectMapChange(){
         //TODO: figure out where this goes for the animations
     }
+
+    @Override
+    public String toString() {
+        String return_string = "\nTHIS IS THE GAME\n\n";
+        return_string += ("\t\t" + Integer.toString(this.game_board.get(2)) + "\n");
+        return_string += ("\t" + Integer.toString(this.game_board.get(1))+ "\t" + Integer.toString(this.game_board.get(5)) + "\t" + Integer.toString(this.game_board.get(6)) + "\n");
+        return_string += (Integer.toString(this.game_board.get(0)) + "\t" + Integer.toString(this.game_board.get(4)) + "\t" + Integer.toString(this.game_board.get(9)) + "\t" + Integer.toString(this.game_board.get(10)) + "\t" + Integer.toString(this.game_board.get(11)) + "\n" );
+        return_string += (Integer.toString(this.game_board.get(3)) + "\t" + Integer.toString(this.game_board.get(8)) + "\t" + Integer.toString(this.game_board.get(13)) + "\t" + Integer.toString(this.game_board.get(14)) + "\t" + Integer.toString(this.game_board.get(15)) + "\n" );
+        return_string += (Integer.toString(this.game_board.get(7)) + "\t" + Integer.toString(this.game_board.get(12)) + "\t" + Integer.toString(this.game_board.get(16)) + "\t" + Integer.toString(this.game_board.get(17)) + "\t" + Integer.toString(this.game_board.get(18)) + "\n\n" );
+        return return_string;
+    }
+
+//    public static void main(String[] args){
+//        HexGame game = new HexGame();
+//        int direction;
+//
+//        /*TEST FOR COLLAPSING ON UP*/
+//        direction = 0;
+//        game.game_board.replace(16,2);
+//        game.game_board.replace(13,0);
+//        game.game_board.replace(9 ,2);
+//        game.game_board.replace(5 ,4);
+//        game.game_board.replace(2 ,4);
+//        game.onSwipe(direction);
+//        System.out.println(game.toString());
+//        game.reset();
+//
+//        /*TEST FOR COLLAPSING ON LEFT UP*/
+//        direction = 1;
+//        game.game_board.replace(18,2);
+//        game.game_board.replace(14,0);
+//        game.game_board.replace(9,2);
+//        game.game_board.replace(4,4);
+//        game.game_board.replace(0,4);
+//        game.onSwipe(direction);
+//        System.out.println(game.toString());
+//        game.reset();
+//
+//        /*TEST FOR COLLAPSING ON LEFT DOWN*/
+//        direction = 2;
+//        game.game_board.replace(11,2);
+//        game.game_board.replace(10,0);
+//        game.game_board.replace(9,2);
+//        game.game_board.replace(8,4);
+//        game.game_board.replace(7,4);
+//        game.onSwipe(direction);
+//        System.out.println(game);
+//        game.reset();
+//
+//        /*TEST FOR COLLAPSING ON DOWN*/
+//        direction = 3;
+//        game.game_board.replace(2,2);
+//        game.game_board.replace(5,0);
+//        game.game_board.replace(9,2);
+//        game.game_board.replace(13,4);
+//        game.game_board.replace(16,4);
+//        game.onSwipe(direction);
+//        System.out.println(game);
+//        game.reset();
+//
+//        /*TEST FOR COLLAPSING ON RIGHT DOWN*/
+//        direction = 4;
+//        game.game_board.replace(0,2);
+//        game.game_board.replace(4,0);
+//        game.game_board.replace(9,2);
+//        game.game_board.replace(14,4);
+//        game.game_board.replace(18,4);
+//        game.onSwipe(direction);
+//        System.out.println(game);
+//        game.reset();
+//
+//        /*TEST FOR COLLAPSING ON RIGHT UP*/
+//        direction = 5;
+//        game.game_board.replace(7,2);
+//        game.game_board.replace(8,0);
+//        game.game_board.replace(9,2);
+//        game.game_board.replace(10,4);
+//        game.game_board.replace(11,4);
+//        game.onSwipe(direction);
+//        System.out.println(game);
+//        game.reset();
+//    }
 }
